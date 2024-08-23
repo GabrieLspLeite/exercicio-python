2804d1cd1564132c9f2a03e3aabe57410a65f578
Gabriel Sabino <gabrielleitealtern@gmail.com>
2024-08-23 17:39:24 -0300

    adicionado as respostas a partir das respostas do usuário

--- aula.py
+++ aula.py
@@ -1,5 +1,21 @@
-input("Telefonou para a vítima?\n")
-input("Esteve no local do crime?\n")
-input("Mora perto da vítima?\n")
-input("Devia para a vítima?\n")
-input("Já trabalhou com a vítima?\n")
+respostas = []
+
+respostas.append(input("Telefonou para a vítima?[S/N]\n"))
+respostas.append(input("Esteve no local do crime?[S/N]\n"))
+respostas.append(input("Mora perto da vítima?[S/N]\n"))
+respostas.append(input("Devia para a vítima?[S/N]\n"))
+respostas.append(input("Já trabalhou com a vítima?[S/N]\n"))
+
+contador = 0
+for i in respostas:
+    if i == "s" or i == "S":
+        contador += 1
+
+if contador > 1:
+    print("Você é suspeito")
+elif contador > 2:
+    print("Você é cúmplice")
+elif contador > 4:
+    print("Você é o assassino")
+else:
+    print("Você é inocente")
